8ab899aea0278db9bad5265e13b5c7cf285f1501
Аня <@LAPTOP-5IRPDSG2>
2024-03-08 20:33:36 +0700

    Добавьте файлы проекта.

---
+++ lab3IM/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace lab3IM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Num = new System.Windows.Forms.NumericUpDown();
+            this.kletka = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Num)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(573, 112);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поехали!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите число для правила";
+            // 
+            // Num
+            // 
+            this.Num.Location = new System.Drawing.Point(392, 150);
+            this.Num.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Num.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.Num.Minimum = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+            this.Num.Name = "Num";
+            this.Num.Size = new System.Drawing.Size(160, 22);
+            this.Num.TabIndex = 3;
+            this.Num.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // kletka
+            // 
+            this.kletka.BackColor = System.Drawing.Color.DarkGray;
+            this.kletka.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kletka.Location = new System.Drawing.Point(98, 245);
+            this.kletka.Name = "kletka";
+            this.kletka.Size = new System.Drawing.Size(808, 586);
+            this.kletka.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(788, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 85);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Чистка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 885);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.kletka);
+            this.Controls.Add(this.Num);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Num)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Num;
+        private System.Windows.Forms.Panel kletka;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab3IM/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace lab3IM
+{
+    
+
+    public partial class Form1 : Form
+    {
+        
+        int N = 10, M = 15, number;
+        int i, j;
+        string New, pic;
+
+        int[,] matrix;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            matrix = new int[M, N];//первое значение количество столбцов второе строк
+
+            var grid = new MatrixGrid() 
+            { 
+                Parent = kletka, 
+                Dock = DockStyle.Fill,
+                GridSize = new Size(M, N),
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            grid.CellNeeded += grid_CellNeeded;
+            grid.CellClick += grid_CellClick;
+        }
+        void grid_CellClick(object sender, MatrixGrid.CellClickEventArgs e)
+        {
+            matrix[e.Cell.X, e.Cell.Y] = 1 - matrix[e.Cell.X, e.Cell.Y];
+        }
+
+        void grid_CellNeeded(object sender, MatrixGrid.CellNeededEventArgs e)
+        {
+            e.BackColor = matrix[e.Cell.X, e.Cell.Y] == 1 ? Color.Blue : Color.White;
+        }
+
+        private void button2_Click(object sender, EventArgs e)//кнопка очистки, если что чистка рисунка задерживается(может это только на ноутбуке) нужно ждать
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        Random rand = new Random();
+
+        private void btRandom_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = rand.Next(0, 2);//заполнение от 0 до 1 //i-столбец, 0-строка, реализацию брала в интернете, там отдельным классом сделали matrixGrid, у них строки со столбцами перепутаны
+            }
+
+            Invalidate(true);
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //START
+
+            string picHelp;        
+
+            for (i = 0; i < M; ++i)
+            {
+                if (matrix[i, 0] == 1)
+                {
+                    pic = pic + "1";
+                }
+                else
+                {
+                    pic = pic + "0";
+
+                }
+            }
+           
+            for (j = 0; j < N-1; ++j)
+            {
+                picHelp = pic;
+                pic = String.Empty;
+                New = "";
+
+                for (i = 0; i < M; ++i)
+                {
+                    if (i == 0)
+                    {
+                        Rule(picHelp[picHelp.Length - 1], picHelp[i], picHelp[i + 1]);
+                        if (New[i] == '1')
+                        {
+                            matrix[i, j + 1] = 1;
+                            pic = pic + "1";
+                        }
+                        else
+                        {
+                            matrix[i, j + 1] = 0;
+                            pic = pic + "0";
+                        }
+                    }
+                    else
+                    if (i == picHelp.Length - 1)
+                    {
+                        Rule(picHelp[i - 1], picHelp[i], picHelp[0]);
+                        if (New[i] == '1')
+                        {
+                            matrix[i, j + 1] = 1;
+                            pic = pic + "1";
+                        }
+                        else
+                        {
+                            matrix[i, j + 1] = 0;
+                            pic = pic + "0";
+                        }
+                    }
+                    else
+                    {
+                        Rule(picHelp[i - 1], picHelp[i], picHelp[i + 1]);
+                        if (New[i] == '1')
+                        {
+                            matrix[i, j + 1] = 1;
+                            pic = pic + "1";
+                        }
+                        else
+                        {
+                            matrix[i, j + 1] = 0;
+                            pic = pic + "0";
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Rule(char x, char y, char z)
+        {
+            number = (int)Num.Value;
+            string numberBin = Convert.ToString(number, 2);
+
+            while (numberBin.Length < 8)
+            {
+                numberBin = "0" + numberBin;
+            }
+
+            string three = x.ToString() + y.ToString() + z.ToString();
+
+            if (three == "111")
+            {
+                New = New + numberBin[0];
+            }
+            else
+                if (three == "110")
+            {
+                New = New + numberBin[1];
+            }
+            else
+                if (three == "101")
+            {
+                New = New + numberBin[2];
+            }
+            else
+                if (three == "100")
+            {
+                New = New + numberBin[3];
+            }
+            else
+                if (three == "011")
+            {
+                New = New + numberBin[4];
+            }
+            else
+                if (three == "010")
+            {
+                New = New + numberBin[5];
+            }
+            else
+                if (three == "001")
+            {
+                New = New + numberBin[6];
+            }
+            else
+                if (three == "000")
+            {
+                New = New + numberBin[7];
+            }
+        }
+
+        //Сначала попробовала через dataGridView
+
+        //public void DrawLinePoint(PaintEventArgs e)
+        //{
+        //    dataGridView1.RowCount = N;
+        //    dataGridView1.ColumnCount = M;
+        //    int i, j;
+
+        //    //START
+
+        //    for (i = 0; i < M; ++i)
+        //    {
+        //        if (i == 6 || i == 7 || i == 11)//заполнение ячеек цветом(цифрами) вызывая правило 110)
+        //        {
+        //            dataGridView1.Rows[0].Cells[i].Style.BackColor = Color.Blue;
+        //        }
+        //        else
+        //        {
+        //            dataGridView1.Rows[0].Cells[i].Style.BackColor = Color.White;
+        //        }
+        //    }
+
+
+
+        //    for (i = 0; i < M; ++i)
+        //    {
+        //        if (dataGridView1.Rows[0].Cells[i].Style.BackColor == Color.Blue)//заполнение ячеек цветом(цифрами) вызывая правило 110)
+        //        {
+        //            pic = pic + "1";
+        //        }
+        //        else
+        //        {
+        //             pic = pic + "0";
+        //        }
+        //    }
+
+        //    for (j = 0; j < N; ++j)
+        //    {
+        //        for (i = 0; i < M; ++i)
+        //        { 
+        //            if(i == 0)
+        //            {
+        //                Rule(pic[pic.Length - 1], pic[i], pic[i + 1]);
+        //                if (New[i] == '1')//заполнение ячеек цветом(цифрами) вызывая правило 110)
+        //                {
+        //                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Blue;
+        //                }
+        //                else
+        //                {
+        //                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;
+        //                }
+        //            }
+        //            else
+        //            if(i == pic.Length - 1)
+        //            {
+        //                Rule(pic[i - 1], pic[i], pic[0]);
+        //                if (New[i] == '1')//заполнение ячеек цветом(цифрами) вызывая правило 110)
+        //                {
+        //                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Blue;
+        //                }
+        //                else
+        //                {
+        //                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                Rule(pic[i - 1], pic[i], pic[i + 1]);
+        //                if (New[i] == '1')//заполнение ячеек цветом(цифрами) вызывая правило 110)
+        //                {
+        //                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Blue;
+        //                }
+        //                else
+        //                {
+        //                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;
+        //                }
+        //            }
+
+        //        }
+        //    }
+        //}
+
+
+
+    }
+
+}
+
